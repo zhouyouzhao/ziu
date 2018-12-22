8c829c0b9bb74ad882401522358619355fcbe2ed
master <gary.zhou@libzz.com>
2018-12-23 00:35:17 +0800

    add commander ziu img

--- config/index.js
+++ config/index.js
@@ -2,7 +2,7 @@ module.exports = {
     tempDir: '.ziu-templates',
     githubUseName: 'ziu-templates',
     // listApi: 'https://gitee.com/api/v5/users/ziu-templates/repos',
-    listApi: 'https://api.github.com/users/ziu-templates/repos?access_token=def57247cee8cef41dcbfa4f24cfc084092d3fe7',
+    listApi: 'https://api.github.com/users/ziu-templates/repos?access_token=f604129391c2916e7573e72b516d23f13ad95396',
     ziuInfoApi: 'https://registry.npmjs.org/ziu',
     lintType: ['standard', 'airbnb'],
     commanderType: {
